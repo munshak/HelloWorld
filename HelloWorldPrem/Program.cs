89c94da0c475edd9c2da26d6356af00de7713ff6
Munshak <munshak@gmail.com>
2017-08-07 13:26:45 +0530

    Hello World generated in Console

--- HelloWorldPrem/Program.cs
+++ HelloWorldPrem/Program.cs
@@ -10,6 +10,8 @@ namespace HelloWorldPrem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
